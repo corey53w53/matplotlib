3dd664ce8a823176eec80f04d258d1aaf7908d35
Corey Wang <corefire2004@gmail.com>
2022-04-26 00:21:58 -0400

    numpy

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
+a=np.sort([1,3,2,4,6,5])
+
+print(type(a))
 x_values=[]
 y_values=[]
 for num in range(1,50):
@@ -7,4 +11,4 @@ for num in range(1,50):
     y_values.append(num**2)
 
 plt.plot(x_values,y_values)
-plt.show()
+# plt.show()
