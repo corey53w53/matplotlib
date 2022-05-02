7aebcff16e1c0f98a43ef0ee5d51958867ac3938
Corey Wang <corefire2004@gmail.com>
2022-05-02 05:03:34 -0400

    scatter

--- test.py
+++ test.py
@@ -2,14 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-x = np.linspace(0, 2, 100)  # Sample data.
+# x = np.linspace(0, 2, 100)  # Sample data.
 
-plt.figure(figsize=(5, 2.7), layout='constrained')
-plt.plot(x, x, label='linear')  # Plot some data on the (implicit) axes.
-plt.plot(x, x**2, label='quadratic')  # etc.
-plt.plot(x, x**3, label='cubic')
-plt.xlabel('x label')
-plt.ylabel('y label')
-plt.title("Simple Plot")
-plt.legend()
+# plt.figure(figsize=(5, 2.7), layout='constrained')
+# plt.plot(x, x, label='linear')  # Plot some data on the (implicit) axes.
+# plt.plot(x, x**2, label='quadratic')  # etc.
+# plt.plot(x, x**3, label='cubic')
+# plt.xlabel('x label')
+# plt.ylabel('y label')
+# plt.title("Simple Plot")
+# plt.legend()
+# plt.show()
+np.random.seed(19680801)  # seed the random number generator.
+data = {'a': np.arange(50),
+        'c': np.random.randint(0, 50, 50),
+        'd': np.random.randn(50)}
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+
+fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+ax.scatter('a', 'b', c='c', s='d', data=data)
+ax.set_xlabel('entry a')
+ax.set_ylabel('entry b')
 plt.show()
